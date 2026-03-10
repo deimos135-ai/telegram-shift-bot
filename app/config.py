a3d9b63d6afe2e52954782d61bff36d6a9de864a
deimos135-ai <ubnt5000@gmail.com>
2026-03-10 13:39:49 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,10 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-GROUP_CHAT_ID = os.getenv("GROUP_CHAT_ID", "")
-GOOGLE_SHEET_ID = os.getenv("GOOGLE_SHEET_ID", "")
-GOOGLE_WORKSHEET_NAME = os.getenv("GOOGLE_WORKSHEET_NAME", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+GROUP_CHAT_ID = os.getenv("GROUP_CHAT_ID")
+
+GOOGLE_SHEET_ID = os.getenv("GOOGLE_SHEET_ID")
+GOOGLE_WORKSHEET_NAME = os.getenv("GOOGLE_WORKSHEET_NAME")
+
 TIMEZONE = os.getenv("TIMEZONE", "Europe/Kyiv")
 SEND_TIME = os.getenv("SEND_TIME", "09:00")
