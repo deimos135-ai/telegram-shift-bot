f2f84c8876af05469a4840a9e8a2758556c240a2
deimos135-ai <ubnt5000@gmail.com>
2026-03-10 11:19:11 +0200

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,9 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+GROUP_CHAT_ID = os.getenv("GROUP_CHAT_ID")
+
+GOOGLE_SHEET_NAME = os.getenv("GOOGLE_SHEET_NAME")
+
+TIMEZONE = "Europe/Kyiv"
+SEND_TIME = "09:00"
